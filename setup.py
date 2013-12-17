4fe6138d839d6333cc04057f58826b087bdffe23
jruimartins <devnull@localhost>
2013-12-17 11:44:18 +0000

    version increment

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 
 setup(name='StoryStream SkyNet',
-      version='0.3.6',
+      version='0.3.7',
       author='Bite Studio',
       description='Library for accessing the StoryStream API',
       packages=['storystream_skynet'],
